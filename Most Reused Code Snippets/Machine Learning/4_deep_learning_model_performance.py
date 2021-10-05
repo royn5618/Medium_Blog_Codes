e2956163d456018e6ef9ba1d6ddbde5c6f7a3098
Roy <33963924+royn5618@users.noreply.github.com>
2021-10-05 11:01:46 +0100

    Update 4_deep_learning_model_performance.py

--- Most Reused Code Snippets/Machine Learning/4_deep_learning_model_performance.py
+++ Most Reused Code Snippets/Machine Learning/4_deep_learning_model_performance.py
@@ -11,19 +11,30 @@ from tensorflow.keras.layers import Embedding
 from keras.layers import Flatten
 
 # construct your deep_leraning model
-def get_deep_learning_model(vocab_size, max_length):
-    model = Sequential()
-    model.add(Embedding(vocab_size, 10, input_length=max_length))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+def get_deep_learning_model(params):
+    # Define your model using the model params
+    pass
     return model
 
-def get_model_performance(best_model_file_name, vocab_size, max_length):
+def get_model_performance(best_model_file_name, patience, epoch, params):
+    """ 
+    Typically I alter patience
+    Attributes
+    ----------
+    best_model_file_name : str
+        File name to save the model
+    patience : int
+        number of epoch to try for improvment after the last model improvement
+    epoch : int
+        number of epoch in total
+    params : <this is a placeholder for your model paramteres you might 
+              want to use while defining the model in get_deep_learning_model>
+    """
     keras.backend.clear_session()
-    model = get_deep_learning_model(vocab_size, max_length) # Call the model here
+    model = get_deep_learning_model(params)
     callbacks=[
         keras.callbacks.EarlyStopping(monitor="val_loss", 
-                                      patience=15, 
+                                      patience=patience, 
                                       verbose=1, 
                                       mode="min", 
                                       restore_best_weights=True),
@@ -36,11 +47,21 @@ def get_model_performance(best_model_file_name, vocab_size, max_length):
     history = model.fit(X_train, 
                         y_train, 
                         epochs=20,
-                        validation_data=(X_test, y_test), 
+                        validation_data=0.2, 
                         callbacks=callbacks)
     return model, history                
                     
 def plot_performance(history):
+    """ 
+    Plots the Training and Validation Loss
+    
+    Attributes
+    ----------
+    history : Keras History Object
+        History.history attribute is a record of training loss values 
+        and metrics values at successive epochs, as well as validation 
+        loss values and validation metrics values
+    """
     len_keys = len(list(history.history.keys()))
     for i in range(0, len_keys/2):  
         metric_to_plot = list(history.history.keys())[i]
@@ -52,6 +73,6 @@ def plot_performance(history):
     plt.xticks(range(1, max(history.epoch) + 2))
     plt.legend()
     plt.show()
-
-model, history = get_model_performance('best_model_file_name', vocab_size, max_length)
+    
+model, history = get_model_performance('best_model_file_name.sav', vocab_size, max_length)
 plot_performance(history)
